26832d09aaaa511bf8b44d9a00c6426e83dcd336
Matheus Passos <matheus.martinspassos@yahoo.com>
2023-04-05 16:01:23 -0300

    Realizado a proteção do formulário

--- src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -3,19 +3,29 @@ package me.dio.academia.digital.entity.form;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AlunoForm {
-
+  @NotBlank(message = "Preencha o campo corretamente")
+  @Size(min = 3, max = 50, message = "'${validateValue}' Precisa estar entre {min} e {max} caracteres")
   private String nome;
 
+  @NotBlank(message = "Preencha o campo corretamente")
+  @CPF(message = "'${validateValue}' é inválido")
   private String cpf;
 
+  @NotBlank(message = "Preencha o campo corretamente")
+  @Size(min = 3, max = 50, message = "'${validateValue}' Precisa estar entre {min} e {max} caracteres")
   private String bairro;
 
+  @NotBlank(message = "Preencha o campo corretamente")
+  @Size(message = "Data '${validateValue}' é inválida")
   private LocalDate dataDeNascimento;
 }
